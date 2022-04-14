114d1202742efa56fae91023395e9d7532cf9600
briiiqtt <briiiqtt>
2022-04-14 18:07:43 +0900

    lets go home

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 class Subject extends Component{
   render() {
     return (
-      <header>
-        <h1>asdf</h1>
-      </header>
+        <header>
+            <h1>WEB</h1>
+            world wide web!
+        </header>
     );
   }
 }
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!!
+        <Subject/>
     </div>
     )
   }
