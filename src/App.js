13d99b741e5c09dacfc5d9226714b29853c1a568
briiiqtt <briiiqtt>
2022-04-14 17:51:49 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Subject extends Component{
+  render() {
+    return (
+      <header>
+        <h1>asdf</h1>
+      </header>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello, React!!!
+    </div>
+    )
+  }
+}
+
+export default App;
