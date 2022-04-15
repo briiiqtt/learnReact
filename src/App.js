4df7acf37ead0a5951de3f7e1cd9abe73eba4834
briiiqtt <briiiqtt>
2022-04-15 11:28:01 +0900

    component, props, state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
 
-class Subject extends Component{
-  render() {
-    return (
-        <header>
-            <h1>WEB</h1>
-            world wide web!
-        </header>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {
+        title: 'AAAAAAAAAA',
+        sub: 'world wideeeee web'
+      }
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject/>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <Subject title="React" sub="ASDASD"></Subject>
+        <TOC></TOC>
+        <Content title="TITITT" asdf="ASDF"></Content>
     </div>
     )
   }
